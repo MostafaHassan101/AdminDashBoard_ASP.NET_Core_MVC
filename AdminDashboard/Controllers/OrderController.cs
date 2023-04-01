62c20753eabd83fb71f0de38ae8eda2b62f029a8
FATMAAAHMED <fatmaahmedmohamed09@gmai.com>
2023-04-01 07:30:01 +0200

    order controller

---
+++ AdminDashboard/Controllers/OrderController.cs
@@ -0,0 +1,97 @@
+﻿using Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdminDashboard.Controllers
+{
+    public class OrderController : Controller
+      
+    {
+        private readonly DContext _context;
+
+        public OrderController(DContext context)
+        {
+            _context = context;
+        }
+        // GET: OrderController
+        public ActionResult Index()
+        {
+            var orders = _context.Order.Include(o => o.User).Include(o => o.OrderItems).ToList();
+
+            return View(orders);
+        }
+
+        // GET: OrderController/Details/5
+        public ActionResult Details(int id)
+        {
+            var orderitems = _context.OrderDetails
+                .Include("Order").Include("Product").Where(o => o.Order.Id == id);
+            ViewBag.orderitems = orderitems;
+            return View();
+        }
+
+        // GET: OrderController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: OrderController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OrderController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: OrderController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OrderController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: OrderController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ AdminDashboard/Models/OrderModel.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+
+namespace AdminDashboard.Models
+{
+    public class OrderModel
+    {
+        public string Address { set; get; }
+        public decimal TotalPrice { get; set; }
+        public int? Discount { get; set; }
+
+        public string PaymentMethod { get; set; }
+
+        public string UserPhone { get; set; }
+        public virtual ICollection<OrderItems> OrderItems { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
